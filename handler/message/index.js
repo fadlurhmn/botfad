d0e19991c9ae202de6192e34be67e82e533e6dc4
fadlurhmn <47247805+fadlurhmn@users.noreply.github.com>
2020-09-22 08:47:59 +0700

    Update index.js

--- handler/message/index.js
+++ handler/message/index.js
@@ -340,7 +340,7 @@ module.exports = msgHandler = async (client = new Client(), message) => {
             if (!isGroupMsg) return client.reply(from, 'Maaf, perintah ini hanya dapat dipakai didalam grup! [Group Only]', id)
             if (!isGroupAdmins) return client.reply(from, 'Gagal, perintah ini hanya dapat digunakan oleh admin grup! [Admin Group Only]', id)
             const mentions = mentionList(sender.id, botNumber, groupMembers)
-            await client.sendTextWithMentions(from, `Heyy, ${pushname} is calling you !!!\n${mentions}`)
+            await client.sendTextWithMentions(from, `heyy, ${pushname} keluar lo !!!\n${mentions}`)
             break
         case 'botstat': {
             const loadedMsg = await client.getAmountOfLoadedMessages()
